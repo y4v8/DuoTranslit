765e2190f0d21490dc323b9c540185bfe23abf03
y4v8 <y4v8@yandex.ua>
2017-06-23 14:15:38 +0300

    fix

--- duotranslit.user.js
+++ duotranslit.user.js
@@ -2,9 +2,9 @@
 // @name        DuoTranslit
 // @namespace   duolingo
 // @include     https://www.duolingo.com/*
-// @version     1.0.0
-// @updateURL	https://raw.githubusercontent.com/y4v8/DuoTranslit/master/duotranslit.user.js
-// @downloadURL	https://raw.githubusercontent.com/y4v8/DuoTranslit/master/duotranslit.user.js
+// @version     1.0.1
+// @updateURL   https://raw.githubusercontent.com/y4v8/DuoTranslit/master/duotranslit.user.js
+// @downloadURL https://raw.githubusercontent.com/y4v8/DuoTranslit/master/duotranslit.user.js
 // @grant       none
 // ==/UserScript==
 
@@ -13,22 +13,21 @@
 let en = "`qwertyuiop[]asdfghjkl;'zxcvbnm,.~QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>"
 let ru = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
 
-window.onkeydown = e => {
-  if (e.target && e.target.tagName == "TEXTAREA" && e.target.lang) {
+window.addEventListener("keydown", e => {
+  if (e.target && (e.target.tagName == "TEXTAREA" || e.target.tagName == "INPUT" ) && 'lang' in e.target) {
     if (e.target.lang == "ru") {
       translit(e, en, ru)
-    } else if (e.target.lang == "en") {
+    } else {
       translit(e, ru, en)
     }
   }
-}
+}, false)
 
 function translit(e, from, to) {
   let i = from.indexOf(e.key)
   if (i == -1) {
     return
   }
-  e.preventDefault()
 
   let a = e.target
   let start = a.selectionStart
@@ -36,6 +35,36 @@ function translit(e, from, to) {
 
   a.value = a.value.substr(0,start) + to[i] + a.value.substr(end)
 
-  start = start + 1
+  start++
+  a.setSelectionRange(start, start)
+}
+
+window.addEventListener("input", e => {
+  if (e.target && (e.target.tagName == "TEXTAREA" || e.target.tagName == "INPUT" ) && 'lang' in e.target) {
+    if (e.target.lang == "ru") {
+      removeLastChar(e, en, ru)
+    } else {
+      removeLastChar(e, ru, en)
+    }
+  }
+}, true)
+
+function removeLastChar(e, from, to) {
+  let a = e.target
+  let start = a.selectionStart
+  let end = a.selectionEnd
+  
+  if (start != end || start == 0) {
+   return
+  }
+  
+  start--
+  let key = a.value.substr(start, 1)
+  let i = from.indexOf(key)
+  if (i == -1) {
+    return
+  }
+
+  a.value = a.value.substr(0,start) + a.value.substr(end)
   a.setSelectionRange(start, start)
 }
